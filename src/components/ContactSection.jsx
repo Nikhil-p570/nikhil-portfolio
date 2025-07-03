a0cd6263680b3451a236f252d03fe5238f8e2864
Nikhil <nikhil.pabbisetti2006@gmail.com>
2025-07-03 15:02:04 +0530

    linked in link added

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -118,7 +118,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/nikhil-pabbisetti/" target="_blank" rel="noopener noreferrer">
                   <Linkedin />
                 </a>
                 <a href="#" target="_blank" rel="noopener noreferrer">

0cf9b4de1535d4946a29cfe5c3230f2059d02acd
Nikhil Pabbisetti <127665820+Nikhil-p570@users.noreply.github.com>
2025-09-05 00:38:01 +0530

    Update Download CV button link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,9 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/10KoxKKakQSrw7OLyCz4Neb9Lex7-dgKn/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
